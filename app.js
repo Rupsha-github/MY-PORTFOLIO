8b4d3394564c321f93f66f3e75511b0930c7c217
Rupsha-github <rupshasaha3002@gmail.com>
2025-06-07 17:37:10 +0530

    Updated contact section

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ document.querySelectorAll('.card').forEach(card => {
         card.style.setProperty('--y', `${e.clientY - rect.top}px`);
     })
 })
+
+// JS for the project videos will go here
+
+
+
+//Form Handling
+document.getElementById('contactForm').addEventListener(submit, function(e) {
+    e.preventDefault();
+
+    //show success message
+    document.getElementById('successMessage').style.display = 'block';
+
+    //Reset form for new inputs
+    this.reset();
+
+    //Remove success message after 3 seconds
+    setTimeout(function() {
+        document.getElementById('successMessage').style.display = 'none';
+    }, 3000);
+})
